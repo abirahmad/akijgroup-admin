8686cd7c13f6101b10563fa1179ac6344e1b46f8
abirahmad <abir.ahmad007@gmail.com>
2024-08-07 00:07:56 +0600

    update

--- components/department/DepartmentList.tsx
+++ components/department/DepartmentList.tsx
@@ -10,25 +10,25 @@ import NewButton from '@/components/button/button-new';
 import ActionButtons from '@/components/button/button-actions';
 import StatusBadge from '@/components/badge/StatusBadge';
 import NoTableDataFound from '@/components/table/NoDataFound';
-import { RootState, AppDispatch } from '@/redux/store';
+import { RootState } from '@/redux/store';
 import { PageContentList } from '@/components/layouts/PageContentList';
-import { deleteDepartment, emptyDepartmentInputAction, getDepartmentListAction } from '@/redux/actions/department-action';
-import { IDepartmentView } from '@/redux/interfaces';
+import { deleteEmployee, emptyEmployeeInputAction } from '@/redux/actions/employee-action';
+import { IDepartmentView, IEmployeeView } from '@/redux/interfaces';
 import { hasPermission } from '@/utils/permission';
 import PermissionModal from '../permissionModal';
+import { deleteDepartment, emptyDepartmentInputAction, getDepartmentListAction } from '@/redux/actions/department-action';
 
 export default function DepartmentList() {
-    const dispatch = useDispatch<AppDispatch>();
+    const dispatch = useDispatch();
     const router = useRouter();
     const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
     const [departmentID, setDepartmentID] = useState<number | null>(null);
     const [currentPage, setCurrentPage] = useState<number>(1);
     const [dataLimit, setDataLimit] = useState<number>(10);
+    const { departmentList, departmentPaginationData, isLoading, isDeleting } = useSelector((state: RootState) => state.department);
     const [searchText, setSearchText] = useState<string>('');
 
-    const { departmentList, departmentPaginationData, isLoading, isDeleting } = useSelector((state: RootState) => state.department);
-
-    const columnData = [
+    const columnData: any[] = [
         { title: "SL", id: 1 },
         { title: 'Name', id: 2 },
         { title: 'Code', id: 3 },
@@ -41,16 +41,17 @@ export default function DepartmentList() {
         }, 500),
         [currentPage, dataLimit, searchText, dispatch]
     );
-
+    
     useEffect(() => {
         debouncedDispatch();
-        return () => debouncedDispatch.cancel();
+        return debouncedDispatch.cancel;
     }, [debouncedDispatch]);
+
 
     const handleDeleteDepartmentModal = (id: number) => {
         setShowDeleteModal(true);
         setDepartmentID(id);
-    };
+    }
 
     const getActionButtons = (department: IDepartmentView) => {
         const actions = [];
@@ -58,8 +59,10 @@ export default function DepartmentList() {
         if (hasPermission('department.edit')) {
             actions.push({
                 element: 'Edit',
-                onClick: () => router.push(`/department/edit?id=${department.id}`),
-                iconClass: 'pencil',
+                onClick: () => router.push(
+                    `/department/edit?id=${department.id}`
+                ),
+                iconClass: 'pencil'
             });
         }
 
@@ -67,77 +70,80 @@ export default function DepartmentList() {
             actions.push({
                 element: 'Delete',
                 onClick: () => handleDeleteDepartmentModal(department.id),
-                iconClass: 'trash',
+                iconClass: 'trash'
             });
         }
 
         return actions;
-    };
+    }
 
     return (
         <div>
             <PageHeader
-                title="Departments"
-                searchPlaceholder="Search departments..."
+                title={ 'Departments'}
+                searchPlaceholder={`Search departments...`}
                 searchText={searchText}
                 onSearchText={setSearchText}
                 headerRightSide={
                     <NewButton
                         onClick={() => dispatch(emptyDepartmentInputAction())}
-                        href="/department/create"
-                        element="New Department"
+                        href={'/department/create'}
+                        element={'New Department'}
                     />
                 }
             />
 
             <PageContentList>
-                {isLoading ? (
-                    <div className="text-center">
-                        <Loading loadingTitle="Loading departments..." />
-                    </div>
-                ) : (
-                    <Table
-                        column={columnData}
-                        currentPage={currentPage}
-                        setCurrentPage={setCurrentPage}
-                        dataLimit={dataLimit}
-                        totalData={departmentPaginationData.total}
-                    >
-                        {departmentList && departmentList.length > 0 ? (
-                            departmentList.map((department: IDepartmentView, index: number) => (
-                                <tr
-                                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-left"
-                                    key={department.id}
-                                >
-                                    <th scope="row" className="px-2 py-3 font-normal text-gray-900 break-words">
-                                        {index + 1}
-                                    </th>
-                                    <td className="px-2 py-3 font-normal text-gray-900 break-words">
-                                        {department.name}
-                                    </td>
-                                    <td className="px-2 py-3 font-normal text-gray-900 break-words">
-                                        {department.code}
-                                    </td>
-                                    <td className="px-2 py-3 flex gap-1">
-                                        <ActionButtons items={getActionButtons(department)} />
-                                    </td>
-                                </tr>
-                            ))
-                        ) : (
-                            <NoTableDataFound colSpan={4}>No Departments found! Please create one.</NoTableDataFound>
-                        )}
-                    </Table>
-                )}
+                {
+                    isLoading ?
+                        <div className="text-center">
+                            <Loading loadingTitle={'Employees...'} />
+                        </div> :
+                        <Table
+                            column={columnData}
+                            currentPage={currentPage}
+                            setCurrentPage={setCurrentPage}
+                            dataLimit={dataLimit}
+                            totalData={departmentPaginationData.total}
+                        >
+                            {
+                                departmentList && departmentList.length > 0 && departmentList.map((department: IdepartmentView, index: index) => (
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-left" key={department.id}>
+                                        <th scope="row" className="px-2 py-3 font-normal text-gray-900 break-words" >
+                                            {index + 1}
+                                        </th>
+                                        <td className="px-2 py-3 font-normal text-gray-900 break-words" >
+                                            {department.name}
+                                        </td>
+                                        <td className="px-2 py-3 font-normal text-gray-900 break-words" >
+                                            {department.code}
+                                        </td>
+
+                                        <td className="px-2 py-3 flex gap-1">
+                                            <ActionButtons
+                                                items={getActionButtons(department)}
+                                            />
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+
+                            {
+                                departmentList && departmentList.length === 0 &&
+                                <NoTableDataFound colSpan={9}>No Department found ! Please create one.</NoTableDataFound>
+                            }
+                        </Table>
+                }
             </PageContentList>
 
             <PermissionModal
                 show={showDeleteModal}
-                status="warning"
+                status={"warning"}
                 isLoading={isDeleting}
-                loadingText="Deleting Department..."
+                loadingText={`Deleting Department`}
                 handleClose={() => setShowDeleteModal(false)}
                 handleAction={() => dispatch(deleteDepartment(departmentID, setShowDeleteModal))}
             />
-        </div>
-    );
+        </div >
+    )
 }
