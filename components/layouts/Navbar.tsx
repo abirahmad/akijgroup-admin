573aecae4fa63ec483fdcd981d075bd59a539993
abirahmad <abir.ahmad007@gmail.com>
2024-08-07 08:42:00 +0600

    updated

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
 
 import { handleSidebar } from "@/redux/actions/global-action";
-import { RootState, AppDispatch } from "@/redux/store"; // Import AppDispatch
+import { RootState, AppDispatch } from "@/redux/store";
 import { getAuthData, logout } from "@/utils/auth";
 
 export default function Navbar() {
@@ -26,17 +26,7 @@ export default function Navbar() {
                         <a href="" className="text-xl font-bold flex items-center lg:ml-2.5">
                             <Image src={'/images/banner.png'} alt={''} height={80} width={300} unoptimized />
                         </a>
-                        {/* <form action="#" method="GET" className="hidden lg:block lg:pl-32">
-                            <label htmlFor="topbar-search" className="sr-only">Search</label>
-                            <div className="mt-1 relative lg:w-64">
-                                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                                    <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" /></svg>
-                                </div>
-                                <input type="text" name="email" id="topbar-search" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full pl-10 p-2.5" placeholder="Search" />
-                            </div>
-                        </form> */}
                     </div>
-
 
                     {
                         typeof userData !== "undefined" && userData !== null &&
@@ -63,7 +53,6 @@ export default function Navbar() {
                             </Dropdown.Item>
                         </Dropdown>
                     }
-
                 </div>
             </div>
         </div>
